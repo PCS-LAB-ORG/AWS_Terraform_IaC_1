2551d4062f7ea8c7fdf918960a0f5baa956235ee
pvondemhagen <pvondemhagen@yahoo.com>
2023-02-13 18:17:13 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,16 +35,6 @@ resource "aws_security_group" "Ec2_security1" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    git_commit           = "538f8b9cfe37c00e298989bf1de581d8a6dddc6d"
-    git_file             = "main.tf"
-    git_last_modified_at = "2023-02-14 02:15:31"
-    git_last_modified_by = "pvondemhagen@yahoo.com"
-    git_modifiers        = "pvondemhagen"
-    git_org              = "PANW-Learning"
-    git_repo             = "AWS_Terraform_IaC_1"
-    yor_trace            = "3cfc9ffc-3f1b-4f62-bdd6-85cd4e287d98"
-  }
 }
 
 resource "aws_instance" "app_server" {
@@ -53,6 +43,5 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name      = "DriftDetectionAppServerInstance"
-    yor_trace = "563ca6cd-7c0f-4379-91b4-76efd6ad588f"
   }
 }
