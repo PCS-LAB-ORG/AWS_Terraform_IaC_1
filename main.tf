119dedd740418ec194d137e98b6e10fada7fad18
pvondemhagen <pvondemhagen@yahoo.com>
2023-02-13 18:11:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,51 +9,15 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-variable "region" {
-  default = "us-west-2"
-}
-
 provider "aws" {
-  region     = "us-west-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region  = "us-west-2"
 }
 
-resource "aws_security_group" "Ec2_security1" {
-  name        = "TerraformEc2_security1"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-05ae32088bccb6d55"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name      = "PrismaDriftDetectionDemo_SecurityGroups"
-  }
-}
-
-resource "aws_instance" "Prisma_Cloud_demo_server" {
+resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name      = "PrismaDriftDetectionDemo_EC2"
+    Name = "ExampleAppServerInstance"
   }
 }
