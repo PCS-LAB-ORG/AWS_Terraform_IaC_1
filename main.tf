538f8b9cfe37c00e298989bf1de581d8a6dddc6d
pvondemhagen <pvondemhagen@yahoo.com>
2023-02-13 18:15:31 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,31 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
+}
+
+resource "aws_security_group" "Ec2_security1" {
+  name        = "TerraformEc2_security1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-05ae32088bccb6d55"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "app_server" {
@@ -18,14 +42,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name                 = "ExampleAppServerInstance"
-    git_commit           = "119dedd740418ec194d137e98b6e10fada7fad18"
-    git_file             = "main.tf"
-    git_last_modified_at = "2023-02-14 02:11:06"
-    git_last_modified_by = "pvondemhagen@yahoo.com"
-    git_modifiers        = "pvondemhagen"
-    git_org              = "PANW-Learning"
-    git_repo             = "AWS_Terraform_IaC_1"
-    yor_trace            = "46504b3c-4c28-4217-9d3d-97189e111dc1"
+    Name = "DriftDetectionAppServerInstance"
   }
 }
