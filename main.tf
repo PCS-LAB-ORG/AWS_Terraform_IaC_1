a63753fe030f31af9ed113d2424a4c98e3594f8e
pvondemhagen <pvondemhagen@yahoo.com>
2023-02-13 18:02:51 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,8 +45,7 @@ resource "aws_security_group" "Ec2_security1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name      = "PrismaDriftDetectionDemo_SecurityGroups"
-    yor_trace = "af4607e1-adad-4bf3-8df4-ec7572ad4f89"
+    Name = "PrismaDriftDetectionDemo_SecurityGroups"
   }
 }
 
@@ -55,7 +54,6 @@ resource "aws_instance" "Prisma_Cloud_demo_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name      = "PrismaDriftDetectionDemo_EC2"
-    yor_trace = "9c1d296b-52a0-444d-ba98-05f324e57f81"
+    Name = "PrismaDriftDetectionDemo_EC2"
   }
 }
